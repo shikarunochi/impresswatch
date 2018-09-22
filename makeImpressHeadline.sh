7e63b1d65655cc32d63dd3dc8232baf031536397
shikarunochi <shikarunochi@gmail.com>
2018-09-22 14:42:52 +0900

    delete unneed file

--- makeImpressHeadline.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-PYTHON=$HOME/local/python
-PYTHONPATH=$HOME/local/python/lib/python3.5/site-packages
-LD_LIBRARY_PATH=$HOME/local/python/lib
-
-/home/hamkumas/local/python/bin/python3 /home/hamkumas/work/impresswatch.py > ~/www/shikarunochi/x/ImpressWatchHeadline.html
